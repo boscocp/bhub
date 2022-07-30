39b63378e26444ea34f6728ed202809149539588
Adalberto Pereira <adalbertobosco@gmail.com>
2022-07-30 11:43:14 -0300

    teste basico modelo cliente

--- backend/bhub/clientes/models.py
+++ backend/bhub/clientes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    razao_social = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    faturamento_declarado = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+    
+class DadosBancarios(models.Model):
+    agencia = models.PositiveSmallIntegerField()
+    conta = models.PositiveSmallIntegerField()
+    banco = models.CharField(max_length=100)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
--- backend/bhub/clientes/tests/test_models.py
+++ backend/bhub/clientes/tests/test_models.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from clientes.models import Cliente
 
 # Create your tests here.
+
+class ClienteModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Cliente.objects.create(
+                razao_social = 'Casado', 
+                telefone = '112222-222',
+                endereco = 'Ali na rua',
+                faturamento_declarado = 1000,               
+            )
+        
+        
+    def test_razao_social(self):
+        cliente = Cliente.objects.get(telefone='112222-222')
+        field_label = cliente._meta.get_field('razao_social').verbose_name
+        self.assertEquals(field_label, 'razao social')
+        
+    def test_data_cadastro_not_null(self):
+        cliente = Cliente.objects.get(telefone='112222-222')
+        self.assertIsNotNone(cliente.data_cadastro)
